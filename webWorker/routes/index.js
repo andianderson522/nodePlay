6736a360092389f3f609c52b1a7bcf2143a6f1b1
andianderson522 <anaki808built@gmail.com>
2015-03-09 22:15:03 -0400

    handle validation

--- webWorker/routes/index.js
+++ webWorker/routes/index.js
@@ -5,10 +5,36 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-router.post('/', function(req, res) {
+router.post('/put-cut', function(req, res) {
 	req.logger.info('in post');
-	res.render('success', { title: 'YEAH'});
-
+	req.logger.info(req.body.path);
+	res.setHeader('Content-Type', 'application/json');
+	if (isEmpty(req.body)) {
+		res.end(JSON.stringify({ error: 'missing post body' }));
+	}
+	req.logger.info(req.body.width);
+	req.logger.info(req.body.height);
+	
+    res.end(JSON.stringify({ success: true }));
 });
+
+function isEmpty(obj) {
+    // null and undefined are "empty"
+    if (obj == null) return true;
+ 
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length && obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+ 
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and toValue enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+ 
+    return true;
+}
 
 module.exports = router;
