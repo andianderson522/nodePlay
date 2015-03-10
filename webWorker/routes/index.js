7eb7c9da821cbc8eb2dc55d56cb64f82beb85470
andianderson522 <anaki808built@gmail.com>
2015-03-09 22:19:30 -0400

    ping route

--- webWorker/routes/index.js
+++ webWorker/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/ping', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify({ pong: true}));
 });
 router.post('/put-cut', function(req, res) {
 	req.logger.info('in post');
