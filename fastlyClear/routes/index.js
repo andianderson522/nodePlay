69c7cc75c50258e8a8282b20549078916c35cfbe
andianderson522 <anaki808built@gmail.com>
2015-03-17 13:44:30 -0400

    stub form and doPost

--- fastlyClear/routes/index.js
+++ fastlyClear/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Style Utilities' });
+  res.render('index', { title: 'Fastly Clear Form' });
+});
+router.post('/', function(req, res) {
+	res.render('index', { title: 'Fastly Clear Form Success' });
 });
 
 module.exports = router;
